5a11441c830d701cfb80c0b93ff232b80c6d808d
tqcuong1997 <rairiken@gmail.com>
2018-03-06 17:48:43 +0700

    Bai2

---
+++ Bai2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("IP Address: ");
+            string ip = Console.ReadLine();
+            IPHostEntry iphe = Dns.Resolve(ip);
+            Console.WriteLine("IP Address: "+ ip);
+            Console.WriteLine("Host: "+ iphe.HostName);
+        }
+    }
+}
