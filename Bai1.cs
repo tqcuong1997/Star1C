696110104e4d99f96ea7e9e9990ed3afbb13ebd9
tqcuong1997 <rairiken@gmail.com>
2018-03-06 17:47:19 +0700

    bai1

---
+++ Bai1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten Host: ");
+            string host = Console.ReadLine();
+            IPHostEntry iphe = Dns.GetHostEntry(host);
+            Console.WriteLine("Ten: ", host);
+            foreach(IPAddress ip in Dns.GetHostAddresses("google.com"))
+            {
+                Console.WriteLine(ip.ToString());
+            }
+        }
+    }
+}
